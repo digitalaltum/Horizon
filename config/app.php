74a75a356024652a45544574f8f4518b4164ee1a
rmunate <raulunatecastro@MacBook-Pro-de-Raul.local>
2023-10-10 09:33:54 -0500

    Initial Commit

---
+++ config/app.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'name' => env("APP_NAME", "ANUB FRAMEWORK"),
+
+    'env' => env("APP_ENV", "LOCAL"),
+
+    'debug' => env("APP_DEBUG", false),
+
+    'url' => env("APP_URL", "http://localhost"),
+
+    'version' => env("APP_VERSION", "http://localhost"),
+
+];
