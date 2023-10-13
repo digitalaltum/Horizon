89125cd104213fe6b92744b3e3fc3b0bbe873317
rmunate <raulunatecastro@MacBook-Pro-de-Raul.local>
2023-10-13 09:04:37 -0500

    CL SKELETON

---
+++ bootstrap/cache/config.php
@@ -0,0 +1,9 @@
+<?php return array (
+  'base_root' => '/Users/raulunatecastro/Documents/LocalHost/Horizon Stack/Horizon-Skeleton',
+  'start_app' => 1697201214.422056,
+  'name' => 'HORIZON FRAMEWORK',
+  'env' => 'LOCAL',
+  'debug' => 'true',
+  'url' => 'http://localhost',
+  'version' => '0.1',
+);
